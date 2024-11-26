1410c5b1e73721302457021201e9f5d1582987b6
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-11-26 10:06:27 +0100

    ported fixes from FIS VIVO

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/search/controller/PagedSearchController.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/search/controller/PagedSearchController.java
@@ -373,6 +373,7 @@ public class PagedSearchController extends FreemarkerHttpServlet {
                 FilterValue filterValue = searchFilter.getValue(valueName);
                 if (filterValue == null) {
                     filterValue = new FilterValue(valueName);
+                    filterValue.setDisplay(true);
                     searchFilter.addValue(filterValue);
                 }
                 if (requestFiltersById.containsKey(searchFilter.getId())) {
